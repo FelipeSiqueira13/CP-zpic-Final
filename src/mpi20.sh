4b5d70328c40449aa3c3a2992601728798afa776
Felipe <fsiqesp2@gmail.com>
2026-01-12 14:05:56 +0000

    Update mpi20.sh

--- src/mpi20.sh
+++ src/mpi20.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=f202500010hpcvlabuminhoa
 #SBATCH --partition=normal-arm
-#SBATCH --ntasks=16
+#SBATCH --ntasks=20
 #SBATCH --time=00:4:00
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
