3d20a75f3e1aa831c1684ce6950953e1659e907d
Felipe <fsiqesp2@gmail.com>
2026-01-12 21:55:16 +0000

    Update simulation.c

--- src/simulation.c
+++ src/simulation.c
@@ -53,7 +53,7 @@ void sim_iter( t_simulation* sim) {
 
 	// Sum current across all MPI ranks (after all species have deposited)
 	int jlen = (sim->current.gc[0] + sim->current.nx + sim->current.gc[1]) * 3;
-	MPI_Allreduce(MPI_IN_PLACE, sim->current.J_buf, jlen, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+	MPI_Reduce(MPI_IN_PLACE, sim->current.J_buf, jlen, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
 	if(rank != 0) return;
 	// Update current boundary conditions and advance iteration
 	current_update( &sim -> current );
